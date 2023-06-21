227c8c37d75a7495096de3486e5ba7ef95397eb2
kkhtun <khaingkhanthtun@gmail.com>
2023-06-21 20:49:10 +0630

    fix env mistake in client

--- client/src/environment.js
+++ client/src/environment.js
@@ -1,6 +1,6 @@
 export const environment = {
     url:
         import.meta.env.MODE === "production"
-            ? "http://ec2-18-141-187-164.ap-southeast-1.compute.amazonaws.com/"
+            ? "http://ec2-18-141-187-164.ap-southeast-1.compute.amazonaws.com"
             : "http://localhost:3000",
 };
