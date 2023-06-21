83f3101b172b395e2d4f5061f0789e7d6d033897
kkhtun <khaingkhanthtun@gmail.com>
2023-06-21 19:51:46 +0630

    added pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [
+        {
+            script: "server.js",
+            watch: ".",
+            instances: "max",
+            max_memory_restart: "256M",
+            env: {
+                NODE_ENV: "development",
+            },
+            env_production: {
+                NODE_ENV: "production",
+            },
+        },
+        {
+            script: "./service-worker/",
+            watch: ["./service-worker"],
+        },
+    ],
+};
---
+++ tools/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git pull
+npm install
+pm2 reload ecosystem.config.js --env production
+# EOF
