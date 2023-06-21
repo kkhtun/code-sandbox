f907413f11e83001e0ba6e4c9edfa9b80a61b10e
kkhtun <khaingkhanthtun@gmail.com>
2023-06-21 20:07:38 +0630

    config vite production build url

--- client/src/environment.js
+++ client/src/environment.js
@@ -1,3 +1,6 @@
 export const environment = {
-    url: "http://localhost:3000",
+    url:
+        import.meta.env.MODE === "production"
+            ? "http://ec2-18-141-187-164.ap-southeast-1.compute.amazonaws.com/"
+            : "http://localhost:3000",
 };
