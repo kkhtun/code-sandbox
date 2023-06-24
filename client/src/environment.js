f8d922e6b399b04f0c3157653e162832d0efc826
kkhtun <khaingkhanthtun@gmail.com>
2023-06-24 12:03:11 +0630

    change env

--- client/src/environment.js
+++ client/src/environment.js
@@ -1,6 +1,6 @@
 export const environment = {
     url:
         import.meta.env.MODE === "production"
-            ? "http://ec2-3-0-89-152.ap-southeast-1.compute.amazonaws.com"
+            ? "http://ec2-54-169-186-99.ap-southeast-1.compute.amazonaws.com"
             : "http://localhost:3000",
 };
